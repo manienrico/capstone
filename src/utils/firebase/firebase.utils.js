3c5f5736e9ba86886c3bdf92c2e6fefe87c7cafd
Enrico Mani <manienrico13@gmail.com>
2023-06-06 09:57:20 +0300

    firbase configured to signin with google popup

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import {initializeApp} from 'firebase/app'
+import {getAuth,signInWithRedirect,signInWithPopup,GoogleAuthProvider} from 'firebase/auth'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAdFAj-TjuPeVGd4-EWJ0RH8UgzETjnTNw",
+    authDomain: "capstone-db-74adc.firebaseapp.com",
+    projectId: "capstone-db-74adc",
+    storageBucket: "capstone-db-74adc.appspot.com",
+    messagingSenderId: "742377144854",
+    appId: "1:742377144854:web:afecf15e55a45b2f0eab37"
+  };
+  
+  // Initialize Firebase
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const provider = new GoogleAuthProvider()
+  provider.setCustomParameters({
+    // tell how google auth prov to behave
+    prompt: "select_account"
+  })
+
+  export const auth = getAuth()
+  export const signInWithGooglePopup = ()=>signInWithPopup(auth,provider)
