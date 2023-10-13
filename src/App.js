260070fccf74e4e63a509384fed0fffec7393251
Enrico Mani <manienrico13@gmail.com>
2023-10-13 13:04:07 +0300

    created checkout page along with checkout items and checkout functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Routes,Route} from 'react-router-dom'
 import Navigation from "./components/routes/navigation/navigation.component";
 import Authentication from "./components/routes/authentication/authentication.component";
 import Shop from "./components/routes/shop/shop.component";
+import Checkout from "./components/routes/checkout/checkout.component";
 
 // const Shop =()=>{
 //   return(
@@ -20,6 +21,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
