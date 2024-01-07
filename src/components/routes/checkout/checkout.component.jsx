cdd0ea27c1701223832aa10c0d8cc6c9c8479efa
Enrico Mani <manienrico13@gmail.com>
2024-01-07 13:33:41 +0300

    styled-component implemented in checkout component

--- src/components/routes/checkout/checkout.component.jsx
+++ src/components/routes/checkout/checkout.component.jsx
@@ -3,38 +3,39 @@ import { useContext } from 'react'
 import { CartContext } from '../../../contexts/cart.context'
 
 import CheckoutItem from '../../../components/checkout-item/checkout-item.component'
-import './checkout.styles.scss'
+
+import{CheckoutContainer,CheckoutHeader,HeaderBlock,Total,HeadText} from './checkout.styles.jsx'
 
 const Checkout = ()=>{
     const { cartItems, cartTotal } = useContext(CartContext)
 
     return(
-        <div className='checkout-container'>
-            <div className='checkout-header'>
-                <div className='header-block'>
-                    <span>Product</span>
-                </div>
-                <div className='header-block'>
-                    <span>Description</span>
-                </div>
-                <div className='header-block'>
-                    <span>Quantity</span>
-                </div>
-                <div className='header-block'>
-                    <span>Price</span>
-                </div>
-                <div className='header-block'>
-                    <span>Remove</span>
-                </div>
-            </div>
+        <CheckoutContainer>
+            <CheckoutHeader>
+                <HeaderBlock>
+                    <HeadText as='span'>Product</HeadText>
+                </HeaderBlock>
+                <HeaderBlock>
+                    <HeadText as='span'>Description</HeadText>
+                </HeaderBlock>
+                <HeaderBlock>
+                    <HeadText as='span'>Quantity</HeadText>
+                </HeaderBlock>
+                <HeaderBlock>
+                    <HeadText as='span'>Price</HeadText>
+                </HeaderBlock>
+                <HeaderBlock>
+                    <HeadText as='span'>Remove</HeadText>
+                </HeaderBlock>
+            </CheckoutHeader>
                 {
                     cartItems.map((cartItem)=>{
                         return (
                             <CheckoutItem key={cartItem.id} cartItem={cartItem} />
                         )})
                 }
-            <span className='total'>Total: ${cartTotal}</span>
-        </div>
+            <Total as='span'>Total: ${cartTotal}</Total>
+        </CheckoutContainer>
     )
 }
 
